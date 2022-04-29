a41ff8ae80c5cedc4cadb471b475d3036fc85a5b
mohitkumar008 <mohitkumar0182000@gmail.com>
2022-04-29 16:53:09 +0530

    dynamic-url

--- includes/define.php
+++ includes/define.php
@@ -1,4 +1,20 @@
 <?php
+// Program to display URL of current page.
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+    $link = "https";
+else $link = "http";
 
-define('SITE_ROOT_URL', 'http://localhost/techverse/');
-define('SITE_IMG_URL', 'http://localhost/techverse/images/');
+// Here append the common URL characters.
+$link .= "://";
+
+// Append the host(domain name, ip) to the URL.
+$link .= $_SERVER['HTTP_HOST'];
+
+// Append the requested resource location to the URL
+$link .= $_SERVER['REQUEST_URI'];
+
+// Print the link
+// echo $link;
+
+define('SITE_ROOT_URL', $link);
+define('SITE_IMG_URL', $link . 'images/');
