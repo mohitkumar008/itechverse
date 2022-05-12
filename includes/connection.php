91a5fca36c9870e5c8e21219b207b52af26f06ed
mohitkumar008 <mohitkumar0182000@gmail.com>
2022-05-12 12:43:48 +0530

    db changes

--- includes/connection.php
+++ includes/connection.php
@@ -1,7 +1,7 @@
 <?php
 
-// $conn = mysqli_connect('localhost', 'root', '', 'itechverse_mohit');
-$conn = mysqli_connect('localhost', 'id17590481_root', 'ItechVerse@748159', 'itechverse');
+$conn = mysqli_connect('localhost', 'root', '', 'itechverse_mohit');
+// $conn = mysqli_connect('localhost', 'id17590481_root', 'ItechVerse@748159', 'itechverse');
 // Check connection
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
