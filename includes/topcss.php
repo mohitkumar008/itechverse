bdfa9a00158800f39b28f14a9ce9df081763b732
mohitkumar008 <mohitkumar0182000@gmail.com>
2022-06-02 11:11:26 +0530

    g-tag changes

--- includes/topcss.php
+++ includes/topcss.php
@@ -1,3 +1,16 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4YTWY1E9G8"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-4YTWY1E9G8');
+</script>
+
 <meta charset="utf-8">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 <meta name="author" content="ChitrakootWeb">
